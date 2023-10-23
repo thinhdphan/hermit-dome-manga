bf6bfa13ad9a79d9f7514dc7dad6dee89f8d86c7
tphan1996 <107013661+thinhdphan@users.noreply.github.com>
2023-10-23 18:02:56 -0500

    First git commit

---
+++ js/app.js
@@ -0,0 +1,46 @@
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+  import { getDatabase, set, ref, get, child } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-database.js";
+  import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-auth.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyCvau2ODAfcEqtIaZGan7rgu7JCjFC0Y1M",
+    authDomain: "hermit-dome-manga.firebaseapp.com",
+    projectId: "hermit-dome-manga",
+    storageBucket: "hermit-dome-manga.appspot.com",
+    messagingSenderId: "437514867361",
+    appId: "1:437514867361:web:e0e7bab05029cccff156e0"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const db = getDatabase();
+  const auth = getAuth(app);
+  const dbref = ref(db);
+  
+  // HTML elements
+  let emailInp = document.getElementById('userEmail');
+  let passInp = document.getElementById('userPassword');
+  let mainForm = document.getElementById('mainForm');
+
+
+  let signInUser = event => {
+    event.preventDefault();
+
+    signInWithEmailAndPassword(auth, emailInp.value, passInp.value).then((credentials) => {
+      get(child(dbref, 'UsersAuthList/' + credentials.user.uid)).then((snapshot) => {
+        if(snapshot.exists){
+          localStorage.setItem("user-info", JSON.stringify({
+            firstname: snapshot.val().firstname,
+            lastname: snapshot.val().lastname
+          }))
+          localStorage.setItem("user-creds", JSON.stringify(credentials.user));
+          window.location.href='manga.html';
+        }
+      })
+    })
+    .catch((error) => {
+      alert('incorrect user/password!');
+    })
+  }
+
+  mainForm.addEventListener('submit', signInUser); 
---
+++ js/register.js
@@ -0,0 +1,46 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+import { getDatabase, set, ref } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-database.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCvau2ODAfcEqtIaZGan7rgu7JCjFC0Y1M",
+  authDomain: "hermit-dome-manga.firebaseapp.com",
+  projectId: "hermit-dome-manga",
+  storageBucket: "hermit-dome-manga.appspot.com",
+  messagingSenderId: "437514867361",
+  appId: "1:437514867361:web:e0e7bab05029cccff156e0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase();
+const auth = getAuth(app);
+const dbref = ref(db);
+
+// HTML Elements
+let emailInput = document.getElementById('emailInp');
+let passInput = document.getElementById('passwordInp');
+let firstNameInput = document.getElementById('uFirstName');
+let lastNameInput = document.getElementById('uLastName');
+let registerBtn = document.getElementById('register-user');
+
+
+// Register User
+let RegisterUser = event => {
+  event.preventDefault();
+
+  createUserWithEmailAndPassword(auth, emailInput.value, passInput.value).then((credentials) => {
+    set(ref(db, 'UsersAuthList/' + credentials.user.uid), {
+      firstname: firstNameInput.value,
+      lastname: lastNameInput.value
+    })
+    
+    alert('You have registered with Hermit Dome Manga!')
+  }) 
+  .catch((error) => {
+    alert('Invalid Registry: Please fill out the entire form before clicking register. Thank you!');
+  })
+
+}
+
+registerBtn.addEventListener('click', RegisterUser); 
